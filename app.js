dc080f8257f9dd32b4327cb6e4dedfdc0ad96aeb
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-23 12:54:18 +0300

    hovered

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
-
-function dragstart(event) {
-    console.log('drag start', event.target);
+for (const placeholder of placeholders) { 
+    placeholder.addEventListener('dragover',dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
 }
 
-function dragend() {
-    console.log('drag end');
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add('hide'), 0)    
+}
+
+function dragend(event) {
+    event.target.className = 'item'
+}
+
+function dragover(event) {
+    console.log('drag over');
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+    console.log('drag enter');
+}
+
+function dragleave(event) {
+    console.log('drag leave');
+}
+
+function dragdrop(event) {
+    console.log('drag drop');
 }
